67b8203d9c3765753e97d625c7655fd7a4815ea6
Mystified131 <mystifiedthomas@gmail.com>
2019-06-04 10:04:13 -0500

    Initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        public IActionResult Index()
+        {
+            IndexViewModel indexViewModel = new IndexViewModel();
+
+            return View(indexViewModel);
+        }
+
+        public IActionResult Index2()
+        {
+            Index2ViewModel index2ViewModel = new Index2ViewModel();
+
+            return View(index2ViewModel);
+        }
+
+        public IActionResult Error()
+        {
+
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            ResultViewModel resultViewModel = new ResultViewModel();
+
+            resultViewModel.Error = "To add a new shape, please return to the 'Add' page.";
+
+
+
+            return View(resultViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Result(ResultViewModel resultViewModel)
+
+
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+
+                    Encrypt mess = new Encrypt(resultViewModel.messin);
+
+                    resultViewModel.messout = mess.Messout(resultViewModel.messin);
+
+                    return View(resultViewModel);
+
+                }
+          
+
+            return Redirect("/Home/Error");
+        }
+
+        public IActionResult Result2()
+        {
+            Result2ViewModel result2ViewModel = new Result2ViewModel();
+
+            result2ViewModel.Error = "To add a new shape, please return to the 'Add' page.";
+
+
+
+            return View(result2ViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Result2(Result2ViewModel result2ViewModel)
+
+
+
+        {
+
+
+            if (ModelState.IsValid)
+            {
+
+                Encrypt mess = new Encrypt(result2ViewModel.decrypt);
+
+                result2ViewModel.messout = mess.Decrypt(result2ViewModel.decrypt);
+
+                return View(result2ViewModel);
+
+            }
+
+
+            return Redirect("/Home/Error");
+        }
+
+
+    }
+
+}
---
+++ WebApplication1/ViewModels/Result2ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ViewModels
+{
+    public class Result2ViewModel
+    {
+        public string messout { get; set; }
+        public string decrypt { get; set; }
+        public String Error { get; set; }
+    }
+}
